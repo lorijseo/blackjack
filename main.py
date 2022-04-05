d42d2683d7d561fd808c5bddadce9b13d77fc37f
lorijseo <lorijseo@gmail.com>
2022-04-04 21:07:33 -0700

    added possibility of aces

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# while blackjack function
-def blackjack_play():
-    import random
+from art import logo
+import random
 
+
+def blackjack_play():
     def player_drawing():
         draw = random.choice(cards)
         player_hand.append(draw)
@@ -10,55 +11,82 @@ def blackjack_play():
         draw = random.choice(cards)
         computer_hand.append(draw)
 
-    # def computer_drawing():
-    #     computer_sum = computer_hand[0]
-    #     while computer_sum < 17:
-    #         draw = random.choice(cards)
-    #         computer_hand.append(draw)
-    #         computer_sum += computer_hand[-1]
-    #     return computer_sum
-
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     player_hand = []
     player_sum = 0
     computer_hand = []
-    # player draws 2 times
+
     for i in range(2):
         player_drawing()
-    # player sum of hand
     for card in player_hand:
         player_sum += card
-    # computer draws 1 time
-    computer_first_draw()
-    # display start of game
-    print(f"Your cards: {player_hand}, current score: {player_sum} \nComputer's first card: {computer_hand}")
-    # asks player to continue drawing
-    continue_play = input("Type 'y' to get another card, type 'n' to pass: ")
 
+    if player_sum > 21:
+        player_hand.remove(11)
+        player_hand.append(1)
+        print("One of your aces is now represented as a value of 1")
+
+    computer_first_draw()
+
+    print(f"Your cards: {player_hand}, current score: {player_sum} \nComputer's first card: {computer_hand}")
+    continue_play = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+    if continue_play == 'y':
+        continue_play = True
+    else:
+        continue_play = False
 
     while continue_play:
         player_drawing()
         player_sum += player_hand[-1]
         if player_sum > 21:
-            # if we have an ace, we can replace with a value of 1.
-            computer_sum = computer_hand[0]
-            while computer_sum < 17:
-                computer_draw = random.choice(cards)
-                computer_hand.append(computer_draw)
-                computer_sum += computer_hand[-1]
-            print(f"Your final hand: {player_hand}, final score: {player_sum} \nComputer's final hand: {computer_hand},"
-                  f"final score: {computer_sum}")
-            print("You went over. You lose!")
-            continue_play = False
+            if 11 in player_hand:
+                ace_location = player_hand.index(11)
+                player_hand.remove(11)
+                player_hand.insert(ace_location, 1)
+                player_sum -= 10
+                print("Your ace is now represented as a value of 1")
+                print(f"Your cards: {player_hand}, current score: {player_sum} \n"
+                      f"Computer's first card: {computer_hand}")
+                continue_play = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+                if continue_play == "n":
+                    continue_play = False
+            else:
+                continue_play = False
         else:
             print(f"Your cards: {player_hand}, current score: {player_sum} \nComputer's first card: {computer_hand}")
             continue_play = input("Type 'y' to get another card, type 'n' to pass: ").lower()
             if continue_play == "n":
                 continue_play = False
 
+    computer_sum = computer_hand[0]
+    while computer_sum < 17:
+        computer_draw = random.choice(cards)
+        computer_hand.append(computer_draw)
+        computer_sum += computer_hand[-1]
+        if computer_sum > 21:
+            if 11 in computer_hand:
+                ace_location = computer_hand.index(11)
+                computer_hand.remove(11)
+                computer_hand.insert(ace_location, 1)
+                computer_sum -= 10
+
+    print(f"Your final hand: {player_hand}, final score: {player_sum} \nComputer's final hand: {computer_hand},"
+          f"final score: {computer_sum}")
+    if player_sum > 21:
+        print("You went over. You lose!")
+    elif computer_sum > 21:
+        print("Computer went over. You win!")
+    elif player_sum > computer_sum:
+        print("You win!")
+    elif player_sum < computer_sum:
+        print("You lose!")
+    else:
+        print("It's a draw!")
+
 
 blackjack = input("Do you want to play a game of Blackjack? Type 'y' or 'n' ").lower()
 if blackjack == 'y':
+    print(logo)
     blackjack_play()
 else:
     print("Byebye")
